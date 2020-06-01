0b110c8db25607c2c112e2206b09d9a16a9ab6c2
catmando <mitch@catprint.com>
2020-06-01 17:35:48 -0400

    changed min height on iframe

--- app/hyperstack/components/give.rb
+++ app/hyperstack/components/give.rb
@@ -10,7 +10,7 @@ class Give < HyperComponent
       title: "Donation Form",
       style: {
         backgroundColor: :white,
-        maxWidth: 600, minWidth: 320, minHeight: 1200,
+        maxWidth: 600, minWidth: 320, minHeight: 150,
         width: '100%', height: '100%',
         border: :none
       }
